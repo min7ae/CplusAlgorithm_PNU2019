6e51c27ef5926dc0ff0f8ca7d2496abab509d70f
min7ae <innersme@gmail.com>
2019-03-08 17:41:30 +0900

    palin 수정

--- palin/palin.cpp
+++ palin/palin.cpp
@@ -6,37 +6,50 @@
 using namespace std;
 
 int chkpalin(string str){
-    int ret = 3;
+    int ret = 1;
+    // center를 chk
     int center = str.length() / 2;
-    int last = str.length() - 1;
-    // 홀수라면
-    if (str.length() % 2 != 0) {
-        int cur = 0;
-        while( ( str.at(cur) == str.at(last - cur) ) || center == cur ){
-            cur++;
-        }
-        if (center == cur) {
-            ret = 1;
+    // curser
+    int cur = 0;
+    // cur를 증가시키는데, center에 도달할때까지
+    // !짝수 홀수때 경우가 똑같을까? 
+    while(cur != center){
+        if(str.at(cur) == str.at(str.length() - cur)){
+           cur++;
+        // 두개가 다르면,?
         } else {
-            
+            //일단 palin은 아니니까
+            ret += 1;
+            break;
         }
-        
-
-        
-    // 짝수라면
-    } else {
-    
     }
-    // ori 순서
-    string ori = str.substr(center);
-
+    // palin아니라면
+    if (ret != 1) {
+        // 앞에서 출발
+        while(cur != center){
+            if(str.at(cur+1) == str.at(str.length() - cur)){
+                cur++;
+            // 두개가 다르면,?
+            } else {
+                // 뒤에서 출발
+                if(str.at(cur) == str.at(str.length() -1 -cur)){
+                cur++;
+                // 두개가 다르면,?
+                } else {
+                    // 끝 palin아님.
+                    ret += 1;
+                    break;
+                }
+            }
+        }
+    }
     return ret;
 }
 
 int main()
 {
-    string fName = "sampleData_palin/3.inp";
-    string oName = "1.out";
+    string fName = "1.inp";
+    // string oName = "1.out";
 
     ifstream openFile(fName.data());
     // ofstream writeFile(oName.data());
@@ -44,7 +57,7 @@ int main()
 
         string line;
         getline(openFile, line);
-        int n = stoi(line);
+        int n = atoi(line.c_str());
         for(int i = 0; i < n; i++)
         {
             getline(openFile, line);
