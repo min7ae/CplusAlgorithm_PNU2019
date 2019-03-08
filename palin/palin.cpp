b43b322378c652e971d34ad9e05bae501b75f1d8
MJKim <innersme@gmail.com>
2019-03-08 17:51:46 +0900

    palin

--- palin/palin.cpp
+++ palin/palin.cpp
@@ -9,12 +9,13 @@ int chkpalin(string str){
     int ret = 1;
     // center를 chk
     int center = str.length() / 2;
+    int last = str.length()-1;
     // curser
     int cur = 0;
     // cur를 증가시키는데, center에 도달할때까지
     // !짝수 홀수때 경우가 똑같을까? 
     while(cur != center){
-        if(str.at(cur) == str.at(str.length() - cur)){
+        if(str.at(cur) == str.at(last - cur)){
            cur++;
         // 두개가 다르면,?
         } else {
@@ -27,12 +28,12 @@ int chkpalin(string str){
     if (ret != 1) {
         // 앞에서 출발
         while(cur != center){
-            if(str.at(cur+1) == str.at(str.length() - cur)){
+            if(str.at(cur+1) == str.at(last - cur)){
                 cur++;
             // 두개가 다르면,?
             } else {
                 // 뒤에서 출발
-                if(str.at(cur) == str.at(str.length() -1 -cur)){
+                if(str.at(cur) == str.at(last -1 -cur)){
                 cur++;
                 // 두개가 다르면,?
                 } else {
@@ -48,23 +49,21 @@ int chkpalin(string str){
 
 int main()
 {
-    string fName = "1.inp";
-    // string oName = "1.out";
+    string fName = "palin.inp";
+    string oName = "palin.out";
 
-    ifstream openFile(fName.data());
-    // ofstream writeFile(oName.data());
+    ifstream openFile(fName);
+    ofstream writeFile(oName);
     if(openFile.is_open()){
-
         string line;
         getline(openFile, line);
         int n = atoi(line.c_str());
         for(int i = 0; i < n; i++)
         {
             getline(openFile, line);
-            cout << chkpalin(line) << "\n";
+            writeFile << chkpalin(line) << "\n";
         }
         openFile.close();
     }
-
     return 0;
 }
