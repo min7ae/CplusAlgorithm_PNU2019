6f509a10f574db63eb3492370eaf682c9e321e3b
min7ae <innersme@gmail.com>
2019-03-25 08:55:30 +0900

    sqrt에 대해 정렬

--- battery/battery.cpp
+++ battery/battery.cpp
@@ -14,12 +14,31 @@ typedef struct _Point{
     :x(_x), y(_y) {}
 } Point;
 
-bool comp(Point pa, Point pb){
-    if(pa.x == pb.x){
-        return pa.y < pb.y;
-    } else {
-        return pa.x < pb.x;
-    }
+bool compXY(Point pa, Point pb){
+    return sqrt(pow(pa.x,2)+pow(pa.y,2)) < sqrt(pow(pb.x,2)+pow(pb.y,2));
+}
+
+// bool compY(Point pa, Point pb){
+//     if(pa.y == pb.y){
+//         return pa.x < pb.x;
+//     } else {
+//         return pa.y < pb.y;
+//     }
+// }
+
+// bool compX(Point pa, Point pb){
+//     if(pa.x == pb.x){
+//         return pa.y < pb.y;
+//     } else {
+//         return pa.x < pb.x;
+//     }
+// }
+
+int chklines(vector<Point> p, int w){
+    int ret = 0;
+    vector<double> lines;
+
+    return ret;
 }
 
 int main()
@@ -27,27 +46,24 @@ int main()
     ifstream ifs;
     ofstream ofs;
 
-    ifs.open("sampleData3/1.inp");
+    ifs.open("1.inp");
     ofs.open("battery.out");
 
     int n, z;
     ifs >> n >> z;
     vector<Point> p;
-    vector<int> lines;
+    vector<double> lines;
 
     for(int i = 0; i < n; i++)
     {
         int x, y;
         ifs >> x >> y;
         p.push_back(Point(x, y));
+        // lines.push_back(sqrt(pow(x,2)+pow(y,2)));
     } 
-    sort(p.begin(), p.end(), comp);
+
     
-    for(int i = 0; i < n; i++)
-    {
-        cout << p[i].x << " " <<  p[i].y << endl;
-    } 
-    
+
     ifs.close();
     ofs.close();
     return 0;
