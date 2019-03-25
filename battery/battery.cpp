2e4428e3e5b158a98a1dfdf1930b51ca30e2e0c8
min7ae <innersme@gmail.com>
2019-03-25 15:07:41 +0900

    p 정렬

--- battery/battery.cpp
+++ battery/battery.cpp
@@ -15,28 +15,11 @@ typedef struct _Point{
 } Point;
 
 bool compXY(Point pa, Point pb){
-    return sqrt(pow(pa.x,2)+pow(pa.y,2)) < sqrt(pow(pb.x,2)+pow(pb.y,2));
+    return pow(pa.x,2)+pow(pa.y,2) < pow(pb.x,2)+pow(pb.y,2);
 }
 
-// bool compY(Point pa, Point pb){
-//     if(pa.y == pb.y){
-//         return pa.x < pb.x;
-//     } else {
-//         return pa.y < pb.y;
-//     }
-// }
-
-// bool compX(Point pa, Point pb){
-//     if(pa.x == pb.x){
-//         return pa.y < pb.y;
-//     } else {
-//         return pa.x < pb.x;
-//     }
-// }
-
-int chklines(vector<Point> p, int w){
-    int ret = 0;
-    vector<double> lines;
+bool chklines(vector<Point> p, int w){
+    bool ret = 0;
 
     return ret;
 }
@@ -52,17 +35,18 @@ int main()
     int n, z;
     ifs >> n >> z;
     vector<Point> p;
-    vector<double> lines;
 
     for(int i = 0; i < n; i++)
     {
         int x, y;
         ifs >> x >> y;
         p.push_back(Point(x, y));
-        // lines.push_back(sqrt(pow(x,2)+pow(y,2)));
     } 
-
     
+    // 원점과 가장 가까운 순으로 정렬
+    sort(p.begin(), p.end(), compXY);
+
+
 
     ifs.close();
     ofs.close();
