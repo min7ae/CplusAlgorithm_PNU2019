33a76477f7e070c27c63d83596a90b757fdfee6d
MJKim <innersme@gmail.com>
2019-03-30 03:39:46 +0900

    case 추가

--- oil/oil.cpp
+++ oil/oil.cpp
@@ -2,19 +2,45 @@
 #include "oil.h"
 using namespace std;
 
+int chkCases(int s, int l){
+    int cen = (s+l)/2;
+    int Left = observe(s, cen);
+    int Right = observe(cen+1, l);
+
+    /*
+    case
+     Left|Right 
+    00000|00110
+    00000|11111
+    00111|11100
+    11111|00000
+    01100|00000
+    */
+
+   if (Left == 0 && Right == 2) return 1;
+   else if (Left == 0 && Right == 1) return 2;
+   else if (Left == 2 && Right == 2) return 3;
+   else if (Left == 1 && Right == 0) return 4;
+   else if (Left == 2 && Right == 0) return 5;
+
+   return -1; // ??
+}
+
+
 int main(){
     initial();
     int N = oil_size();
 
     // start, last, cen
-    // chk: 0, 1, 2
     int s, l, cen, chk;
     s = 0; l = N-1; cen = (s+l)/2;
 
-    //answer
-    int i = 0;
-    int j = N-1;
 
-    oil_report(i, j);
+
+
+
+
+
+    // oil_report(i, j);
     return 0;
 }
